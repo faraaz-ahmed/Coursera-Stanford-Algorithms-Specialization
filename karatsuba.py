8247573b6e4bf1f245694631f2a5f0d636b201db
faraaz-ahmed <faraazahmed000@gmail.com>
2019-09-14 01:43:04 +0530

    first commit

--- karatsuba.py
+++ karatsuba.py
@@ -15,7 +15,7 @@ def karatsuba(x, y):
         return x * y
     else:
         length = len(str(x))
-        position = int(length / 2) if (length%2 == 0) else int(length/2 + 1)
+        position = int(length / 2) if (length % 2 == 0) else int(length / 2 + 1)
         a, b, c, d = int(strx[:int(length / 2)]), int(strx[int(length / 2):]), int(stry[:int(length / 2)]), int(stry[int(length / 2):])
         z0, z1, z2 = karatsuba(a, c), karatsuba(b, d), karatsuba((a + b), (c + d)) 
         return 10 ** (2 * position) * z0 + 10 ** (position) * (z2 - z1 - z0) + z1 
